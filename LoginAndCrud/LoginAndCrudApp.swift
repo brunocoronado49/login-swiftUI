ba82b56c7a54f1c2f74d75e5da17dc908d5dff6a
brunocoronado49 <jofrancoracor@gmail.com>
2021-08-02 19:57:47 -0500

    Initial Commit

---
+++ LoginAndCrud/LoginAndCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAndCrudApp.swift
+//  LoginAndCrud
+//
+//  Created by Bruno Coronado on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginAndCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
